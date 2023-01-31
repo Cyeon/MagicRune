8258bf1bd0bfac5d039ee38a30f3809b53d52979
Cyeon <77601035+Cyeon@users.noreply.github.com>
2023-01-31 15:39:57 +0900

    Help UI Bug Fix

--- Assets/01.Scripts/UI/HelpUI.cs
+++ Assets/01.Scripts/UI/HelpUI.cs
@@ -13,32 +13,42 @@ public enum HelpUIButtons
 
 public class HelpUI : MonoBehaviour
 {
-    //[Header("Buttons")]
-    //public Button backButton = null;
-    //public Button statusButton = null;
-    //public Button interactionButton = null;
+    [Header("Buttons")]
+    public Button backButton = null;
+    public Button onButton = null;
+    public Button statusButton = null;
+    public Button interactionButton = null;
 
     [Header("GameObjects")]
     public GameObject mainPanel = null;
-    public List<GameObject> childPanels = null;
     public GameObject statusTemplate = null;
     public GameObject interactionTemplate = null;
+    public GameObject deckViewUI = null;
+    public GameObject restViewUI = null;
+    public List<GameObject> childPanels = null;
 
     [Header("Contents")]
     public Transform statusContent = null;
     public Transform interactionContent = null;
 
+    private void Awake()
+    {
+        backButton.onClick.AddListener(() => ButtonClick(HelpUIButtons.back));
+        onButton.onClick.AddListener(() => ButtonClick(HelpUIButtons.on));
+        statusButton.onClick.AddListener(() => ButtonClick(HelpUIButtons.status));
+        interactionButton.onClick.AddListener(() => ButtonClick(HelpUIButtons.interaction));
+    }
+
     private void Update()
     {
         if (mainPanel.activeSelf == true && Input.GetKeyDown(KeyCode.Escape))
         {
-            ButtonClick((int)HelpUIButtons.back);
+            ButtonClick(HelpUIButtons.back);
         }
     }
 
-    public void ButtonClick(int idx)
+    public void ButtonClick(HelpUIButtons kind)
     {
-        HelpUIButtons kind = (HelpUIButtons)idx;
         for (int i = 0; i < childPanels.Count; i++)
         {
             childPanels[i].SetActive(false);
@@ -53,10 +63,14 @@ public class HelpUI : MonoBehaviour
                 childPanels[(int)HelpUIButtons.interaction].SetActive(true);
                 break;
             case HelpUIButtons.back:
+                deckViewUI.SetActive(true);
+                restViewUI.SetActive(true);
                 mainPanel.SetActive(false);
                 break;
             case HelpUIButtons.on:
                 mainPanel.SetActive(true);
+                deckViewUI.SetActive(false);
+                restViewUI.SetActive(false);
                 childPanels[(int)HelpUIButtons.status].SetActive(true);
                 break;
             default:
