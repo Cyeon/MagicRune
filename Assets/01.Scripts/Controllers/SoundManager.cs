2bb540a9df36265e527dc312d35da767c7d63771
SimGyuYeong <shgy5901@gmail.com>
2023-01-31 22:31:48 +0900

    Sound, 상태이상 턴 가져오기

---
+++ Assets/01.Scripts/Controllers/SoundManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SoundType
+{
+    Master,
+    Bgm,
+    Effect
+}
+
+public class SoundManager : MonoSingleton<SoundManager>
+{
+    [SerializeField] private AudioSource _master;
+    [SerializeField] private AudioSource _effect;
+    [SerializeField] private AudioSource _bgm;
+
+    private AudioSource _audioSource = null;
+
+    /// <summary>
+    /// 사운드 플레이 함수
+    /// </summary>
+    /// <param name="clip">오디오 클립</param>
+    /// <param name="isLoop">반복 여부</param>
+    public void PlaySound(AudioClip clip, SoundType type, bool isLoop = false)
+    {
+        switch(type)
+        {
+            case SoundType.Master:
+                _audioSource = _master;
+                break;
+
+            case SoundType.Bgm:
+                _audioSource = _bgm;
+                break;
+
+            case SoundType.Effect:
+                _audioSource = _effect;
+                break;
+        }
+
+        _audioSource.Stop();
+        _audioSource.clip = clip;
+        _audioSource.loop = isLoop;
+        _audioSource.Play();
+    }
+
+
+    /// <summary>
+    /// 사운드 정지 함수
+    /// </summary>
+    public void StopSound(SoundType type)
+    {
+        switch (type)
+        {
+            case SoundType.Master:
+                _audioSource = _master;
+                break;
+
+            case SoundType.Bgm:
+                _audioSource = _bgm;
+                break;
+
+            case SoundType.Effect:
+                _audioSource = _effect;
+                break;
+        }
+
+        _audioSource.Stop();
+    }
+}
