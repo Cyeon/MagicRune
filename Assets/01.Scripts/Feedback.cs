c4b3e2020ac8dae8ffd543078ab475a21fa0ea29
SimGyuYeong <shgy5901@gmail.com>
2023-02-01 17:38:42 +0900

    Attack Feedback

---
+++ Assets/01.Scripts/Feedback.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Feedbacks;
+
+public class Feedback : MonoBehaviour
+{
+    public MMFeedback attackFeedback;
+}
