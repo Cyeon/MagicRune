cc9b13de10dea75ae59c92970315a4707a9d963c
SimGyuYeong <shgy5901@gmail.com>
2023-02-08 18:56:21 +0900

    UI 개선

--- Assets/01.Scripts/DummyCost.cs
+++ Assets/01.Scripts/DummyCost.cs
@@ -6,34 +6,57 @@ using TMPro;
 
 public class DummyCost : MonoSingleton<DummyCost>
 {
-    // 싱글턴 패턴도 이런거에서는 안쓸거고
-    
-    // 이건 나중에 만들 땐 다른데서 관리해줘야 할 듯
-    [SerializeField] private TextMeshProUGUI _manaText;
-
     private int _maxCost = 10;
     public int MaxCost => _maxCost;
     [SerializeField]
     private int _nowCost = 10;
     public int NowCost => _nowCost;
 
+    private int _uiCost = 0;
+
+    [SerializeField]
+    private List<GameObject> _emptyManaObjs = new List<GameObject>();
+
     private void Awake()
     {
         EventManager.StartListening(Define.ON_START_PLAYER_TURN, ManaFill);
-        UpdateManaText();
+
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            _emptyManaObjs.Add(transform.GetChild(i).GetChild(0).gameObject);
+        }
+
+        UpdateManaUI();
     }
 
     // 이거는 여기에서 cost를 매개변수로 보내서
     // 텍스트 있는데에서 받아주고 매개변수 받아서 그 값으로 바꿔야 할 듯
-    public void UpdateManaText()
+    public void UpdateManaUI()
     {
-         _manaText.SetText($"{_nowCost}/{_maxCost}");
+        if (_uiCost == _nowCost) return;
+
+        if(_uiCost < _nowCost)
+        {
+            for(int i = _uiCost; i < _nowCost; i++)
+            {
+                _emptyManaObjs[i].SetActive(false);
+            }
+        }
+        else
+        {
+            for(int i = _uiCost; i > _nowCost - 1; i--)
+            {
+                _emptyManaObjs[i].SetActive(true);
+            }
+        }
+
+        _uiCost = _nowCost - 1;
     }
 
     public void ManaFill()
     {
         _nowCost = _maxCost;
-        UpdateManaText();
+        UpdateManaUI();
     }
 
     public bool CanUseMainRune(int cost)
@@ -41,7 +64,7 @@ public class DummyCost : MonoSingleton<DummyCost>
         if (_nowCost >= cost)
         {
             _nowCost -= cost;
-            UpdateManaText();
+            UpdateManaUI();
             return true;
         }
         else
@@ -58,7 +81,7 @@ public class DummyCost : MonoSingleton<DummyCost>
         if (_nowCost >= cost)
         {
             _nowCost -= cost;
-            UpdateManaText();
+            UpdateManaUI();
             return true;
         }
         else
