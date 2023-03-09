a6d8fdc5829cd657754e39df0b232f1a9146fc18
zanke27 <zanke27@gmail.com>
2023-03-09 10:44:29 +0900

    스크롤 바꿈 근데 현재 Y축 움직임 조정 필요함

--- Assets/01.Scripts/Dial/Dummy/DialRotate.cs
+++ Assets/01.Scripts/Dial/Dummy/DialRotate.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DialRotate : MonoBehaviour, IDragHandler
 {
@@ -12,6 +13,7 @@ public class DialRotate : MonoBehaviour, IDragHandler
     private void Awake()
     {
         targetObjTrans = GetComponent<RectTransform>();
+        GetComponent<Image>().alphaHitTestMinimumThreshold = 0.04f;
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -30,7 +32,15 @@ public class DialRotate : MonoBehaviour, IDragHandler
         leftRight *= speed;
 
         Vector3 rot= targetObjTrans.transform.eulerAngles;
-        rot.z += -1 * leftRight;
+        //rot.z += -1 * leftRight;
+
+        float dampValue = Mathf.Abs( eventData.delta.x / 10 );
+
+        Debug.Log(dampValue);
+
+        float dampData = eventData.delta.x / dampValue;
+
+        rot.z += -1 * dampData;
 
         targetObjTrans.transform.rotation = Quaternion.Euler(rot);
 
