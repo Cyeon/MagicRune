bbc68f6471fa3c8ccf8d7c05f330657ce3023a37
SimGyuYeong <shgy5901@gmail.com>
2023-03-20 23:51:41 +0900

    상점 아이템 SO구조 개편

--- Assets/01.Scripts/Map/Shop/ShopItemSO.cs
+++ Assets/01.Scripts/Map/Shop/ShopItemSO.cs
@@ -2,11 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Map/Shop/Item SO")]
+public enum ShopItemType
+{
+    Rune
+}
+
 public class ShopItemSO : ScriptableObject
 {
     public string itemName;
     public int gold;
     public Sprite icon;
-    public ShopItem item;
+    public ShopItemType itemType;
+    private bool _isBuy = false;
+    public bool IsBuy => _isBuy;
+
+    public void Buy()
+    {
+        if (gold > GameManager.Instance.Gold) return;
+        GameManager.Instance.Gold -= gold;
+        _isBuy = true;
+        Execute();
+    }
+
+    public virtual void Execute() { }
 }
