f5e77a279fd5a89b2430a0a6440775f59a2e3b60
SimGyuYeong <shgy5901@gmail.com>
2023-03-23 19:54:11 +0900

    매니저 변경 이슈로 인해 코드 수정

--- Assets/01.Scripts/Reward/RewardPanel.cs
+++ Assets/01.Scripts/Reward/RewardPanel.cs
@@ -16,12 +16,10 @@ public class RewardPanel : MonoBehaviour
         _desc.SetText(reward.desc);
 
         _button.onClick.AddListener(() => reward.GiveReward());
-        _button.onClick.AddListener(() => gameObject.SetActive(false));
-
-        
+        _button.onClick.AddListener(() => ResourceManager.Instance.Destroy(gameObject));
     }
 
-    public void OnPool()
+    public void OnEnable()
     {
         _button = GetComponent<Button>();
         _icon = transform.Find("Icon").GetComponent<Image>();
