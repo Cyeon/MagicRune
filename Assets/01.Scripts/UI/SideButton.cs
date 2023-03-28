ae26e5be0558265b5fda281e0cd00909a1164dcd
YuJeongBin <wjdqls0303@gmail.com>
2023-03-28 19:13:06 +0900

    3/28 턴 종료 버튼 누르는 범위 조절

---
+++ Assets/01.Scripts/UI/SideButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SideButton : MonoBehaviour
+{
+    private Image _image;
+
+    void Start()
+    {
+        _image = GetComponentInChildren<Image>();
+
+        _image.alphaHitTestMinimumThreshold = 0.1f;
+    }
+}
