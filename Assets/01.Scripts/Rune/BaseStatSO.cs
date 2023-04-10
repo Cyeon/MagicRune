35539ba18bc2715901bbe13a1aa4a5c5d1a15aec
JaePil Kim <kjp29816265@gmail.com>
2023-04-10 20:31:04 +0900

    룬 구조 변경하려고 시작하려는 참나

---
+++ Assets/01.Scripts/Rune/BaseStatSO.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseStatSO : MonoBehaviour
+{
+    [SerializeField]
+    protected BaseStatSO _stat;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
