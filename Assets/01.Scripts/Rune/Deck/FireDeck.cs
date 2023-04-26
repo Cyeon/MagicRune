4c17bea2e0cf7e37ec0572c34c4e2593adce43e6
JaePil Kim <kjp29816265@gmail.com>
2023-04-27 00:11:40 +0900

    룬 SO 다시 설정

--- Assets/01.Scripts/Rune/Deck/FireDeck.cs
+++ Assets/01.Scripts/Rune/Deck/FireDeck.cs
@@ -12,7 +12,7 @@ public class FireDeck : Deck
             AddRune(new MagicBullet());
         }
 
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 2; i++)
         {
             AddRune(new Fire());
             AddRune(new FireBreath());
