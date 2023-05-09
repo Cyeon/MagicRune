49aa5c1895e1c288acdad994856b3171df8dd2b4
JaePil Kim <kjp29816265@gmail.com>
2023-05-09 23:27:56 +0900

    스와이프로 움직이게 함
    아마도

--- Assets/01.Scripts/UI/LobbyBGUI.cs
+++ Assets/01.Scripts/UI/LobbyBGUI.cs
@@ -15,14 +15,25 @@ public class LobbyBGUI : MonoBehaviour
     public Ease moveBGEase;
     public float moveBGSpeed;
 
+    private int _index = -1;
+
+    #region Swipe Parameta
+    private Vector2 touchBeganPos;
+    private Vector2 touchEndedPos;
+    private Vector2 touchDif;
+    [SerializeField]
+    private float swipeSensitivity = 5;
+    #endregion
+
     private void Start()
     {
-        bgParent.DOAnchorPosX(1440 * 0, 0).SetEase(moveBGEase);
+        bgParent.DOAnchorPosX(0, 0).SetEase(moveBGEase);
         _selectPanel.DOAnchorPosX(xPosArray[0 + 1], 0).SetEase(moveBGEase);
     }
 
     public void MoveBG(int leftRightToMain)
     {
+        _index = leftRightToMain;
         bgParent.DOAnchorPosX(1440 * leftRightToMain, moveBGSpeed).SetEase(moveBGEase);
         _selectPanel.DOAnchorPosX(xPosArray[leftRightToMain + 1], moveBGSpeed).SetEase(moveBGEase);
     }
@@ -30,5 +41,67 @@ public class LobbyBGUI : MonoBehaviour
     public void GameStart()
     {
         Managers.Scene.LoadScene(Define.Scene.MapScene);
+    }
+
+    private void Update()
+    {
+        Swipe1();
+    }
+
+    public void Swipe1()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                touchBeganPos = touch.position;
+            }
+            if (touch.phase == TouchPhase.Moved)
+            {
+                
+            }
+            if (touch.phase == TouchPhase.Ended)
+            {
+                touchEndedPos = touch.position;
+                touchDif = (touchEndedPos - touchBeganPos);
+
+                if (Mathf.Abs(touchDif.y) > swipeSensitivity || Mathf.Abs(touchDif.x) > swipeSensitivity)
+                {
+                    if (touchDif.y > 0 && Mathf.Abs(touchDif.y) > Mathf.Abs(touchDif.x))
+                    {
+                        //Debug.Log("up");
+                    }
+                    else if (touchDif.y < 0 && Mathf.Abs(touchDif.y) > Mathf.Abs(touchDif.x))
+                    {
+                        //Debug.Log("down");
+                    }
+                    else if (touchDif.x > 0 && Mathf.Abs(touchDif.y) < Mathf.Abs(touchDif.x))
+                    {
+                        //Debug.Log("right");
+                        _index = _index + 1;
+                        if (_index > 2)
+                            _index = -1;
+
+                        MoveBG(_index);
+                    }
+                    else if (touchDif.x < 0 && Mathf.Abs(touchDif.y) < Mathf.Abs(touchDif.x))
+                    {
+                        //Debug.Log("Left");
+                        
+                        _index = _index - 1;
+                        if (_index < -1)
+                            _index = 2;
+
+                        MoveBG(_index);
+                    }
+                }
+                else
+                {
+                    //Debug.Log("touch");
+                }
+            }
+        }
     }
 }
