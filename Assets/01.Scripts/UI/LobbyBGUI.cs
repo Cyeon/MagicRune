9719451a18c29414e5fdedc130f5978e4c49cc87
JaePil Kim <kjp29816265@gmail.com>
2023-05-09 20:29:47 +0900

    아래 selectpanel도 움직이게 함

--- Assets/01.Scripts/UI/LobbyBGUI.cs
+++ Assets/01.Scripts/UI/LobbyBGUI.cs
@@ -2,17 +2,29 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LobbyBGUI : MonoBehaviour
 {
     [SerializeField] RectTransform bgParent;
+    [SerializeField]
+    private RectTransform _selectPanel;
+    [SerializeField]
+    private float[] xPosArray;
 
     public Ease moveBGEase;
     public float moveBGSpeed;
 
+    private void Start()
+    {
+        bgParent.DOAnchorPosX(1440 * 0, 0).SetEase(moveBGEase);
+        _selectPanel.DOAnchorPosX(xPosArray[0 + 1], 0).SetEase(moveBGEase);
+    }
+
     public void MoveBG(int leftRightToMain)
     {
-        bgParent.transform.DOMoveX(1440 * leftRightToMain + 720, moveBGSpeed).SetEase(moveBGEase);
+        bgParent.DOAnchorPosX(1440 * leftRightToMain, moveBGSpeed).SetEase(moveBGEase);
+        _selectPanel.DOAnchorPosX(xPosArray[leftRightToMain + 1], moveBGSpeed).SetEase(moveBGEase);
     }
 
     public void GameStart()
