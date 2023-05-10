1af23a7be26fe106ddd092dc62e1d8a2c28fdcaa
JaePil Kim <kjp29816265@gmail.com>
2023-05-10 18:53:44 +0900

    scrollrect 써서 수정

--- Assets/01.Scripts/UI/LobbyBGUI.cs
+++ Assets/01.Scripts/UI/LobbyBGUI.cs
@@ -1,16 +1,20 @@
 using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
 
 public class LobbyBGUI : MonoBehaviour
 {
+    [SerializeField]
+    private HorizontalScrollSnap _scrollSnap;
     [SerializeField] RectTransform bgParent;
     [SerializeField]
     private RectTransform _selectPanel;
     [SerializeField]
-    private float[] xPosArray;
+    private float[] _xPosArray;
 
     public Ease moveBGEase;
     public float moveBGSpeed;
@@ -18,24 +22,39 @@ public class LobbyBGUI : MonoBehaviour
     private int _index = -1;
 
     #region Swipe Parameta
-    private Vector2 touchBeganPos;
-    private Vector2 touchEndedPos;
-    private Vector2 touchDif;
+    private Vector2 _touchBeganPos;
+    private Vector2 _touchEndedPos;
+    private Vector2 _touchDif;
     [SerializeField]
-    private float swipeSensitivity = 5;
+    private float _swipeSensitivity = 5;
     #endregion
 
     private void Start()
     {
-        bgParent.DOAnchorPosX(0, 0).SetEase(moveBGEase);
-        _selectPanel.DOAnchorPosX(xPosArray[0 + 1], 0).SetEase(moveBGEase);
+        //bgParent.DOAnchorPosX(0, 0).SetEase(moveBGEase);
+        //_selectPanel.DOAnchorPosX(_xPosArray[0 + 1], 0).SetEase(moveBGEase);
+
+        _scrollSnap.OnSelectionPageChangedEvent.AddListener(ChangeIndex);
+        MoveSelectPanel(_scrollSnap.CurrentPage);
     }
 
+    public void ChangeIndex(int index)
+    {
+        _index = index;
+
+        MoveSelectPanel(_index);
+    }
+
+    public void MoveSelectPanel(int leftRightToMain)
+    {
+        _selectPanel.DOAnchorPosX(_xPosArray[leftRightToMain], moveBGSpeed).SetEase(moveBGEase);
+    }
+
+    [Obsolete]
     public void MoveBG(int leftRightToMain)
     {
         _index = leftRightToMain;
         bgParent.DOAnchorPosX(1440 * leftRightToMain, moveBGSpeed).SetEase(moveBGEase);
-        _selectPanel.DOAnchorPosX(xPosArray[leftRightToMain + 1], moveBGSpeed).SetEase(moveBGEase);
     }
 
     public void GameStart()
@@ -45,7 +64,7 @@ public class LobbyBGUI : MonoBehaviour
 
     private void Update()
     {
-        Swipe1();
+        // Swipe1();
     }
 
     public void Swipe1()
@@ -56,7 +75,7 @@ public class LobbyBGUI : MonoBehaviour
 
             if (touch.phase == TouchPhase.Began)
             {
-                touchBeganPos = touch.position;
+                _touchBeganPos = touch.position;
             }
             if (touch.phase == TouchPhase.Moved)
             {
@@ -64,20 +83,20 @@ public class LobbyBGUI : MonoBehaviour
             }
             if (touch.phase == TouchPhase.Ended)
             {
-                touchEndedPos = touch.position;
-                touchDif = (touchEndedPos - touchBeganPos);
+                _touchEndedPos = touch.position;
+                _touchDif = (_touchEndedPos - _touchBeganPos);
 
-                if (Mathf.Abs(touchDif.y) > swipeSensitivity || Mathf.Abs(touchDif.x) > swipeSensitivity)
+                if (Mathf.Abs(_touchDif.y) > _swipeSensitivity || Mathf.Abs(_touchDif.x) > _swipeSensitivity)
                 {
-                    if (touchDif.y > 0 && Mathf.Abs(touchDif.y) > Mathf.Abs(touchDif.x))
+                    if (_touchDif.y > 0 && Mathf.Abs(_touchDif.y) > Mathf.Abs(_touchDif.x))
                     {
                         //Debug.Log("up");
                     }
-                    else if (touchDif.y < 0 && Mathf.Abs(touchDif.y) > Mathf.Abs(touchDif.x))
+                    else if (_touchDif.y < 0 && Mathf.Abs(_touchDif.y) > Mathf.Abs(_touchDif.x))
                     {
                         //Debug.Log("down");
                     }
-                    else if (touchDif.x > 0 && Mathf.Abs(touchDif.y) < Mathf.Abs(touchDif.x))
+                    else if (_touchDif.x > 0 && Mathf.Abs(_touchDif.y) < Mathf.Abs(_touchDif.x))
                     {
                         //Debug.Log("right");
                         _index = _index + 1;
@@ -86,7 +105,7 @@ public class LobbyBGUI : MonoBehaviour
 
                         MoveBG(_index);
                     }
-                    else if (touchDif.x < 0 && Mathf.Abs(touchDif.y) < Mathf.Abs(touchDif.x))
+                    else if (_touchDif.x < 0 && Mathf.Abs(_touchDif.y) < Mathf.Abs(_touchDif.x))
                     {
                         //Debug.Log("Left");
                         
