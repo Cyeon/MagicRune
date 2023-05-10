b772dd64ab7eb4c5376f28e7c0d8747a4af7ece9
Cyeon <77601035+Cyeon@users.noreply.github.com>
2023-05-10 23:27:23 +0900

    챕터 연출 기능 완료
    작동하게 연결해줘야함

---
+++ Assets/01.Scripts/Direction/ChapterTransition.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using DG.Tweening;
+using Cinemachine;
+using System;
+
+public class ChapterTransition : MonoBehaviour
+{
+    private TextMeshProUGUI _chapterNumberText = null;
+    private TextMeshProUGUI _chapterNameText = null;
+
+    private CinemachineStoryboard _storyboard = null;
+
+    private float _wipeSpeed = 0.5f;
+    private float _fadeSpeed = 0.5f;
+
+    private void Start()
+    {
+        _storyboard = FindObjectOfType<CinemachineStoryboard>();
+        _chapterNumberText = transform.Find("ChapterNumberText").GetComponent<TextMeshProUGUI>();
+        _chapterNameText = transform.Find("ChapterNameText").GetComponent<TextMeshProUGUI>();
+    }
+
+    public void Transition()
+    {
+        _chapterNumberText.SetText("Chapter " + Managers.Map.Chapter.ToString());
+        _chapterNameText.SetText(Managers.Map.CurrentChapter.chapterName);
+        StartCoroutine(Wipe());
+    }
+
+    private IEnumerator Wipe()
+    {
+        _storyboard.m_SplitView = 1;
+        while (_storyboard.m_SplitView >= 0)
+        {
+            _storyboard.m_SplitView -= _wipeSpeed * Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        FadeText(1f);
+        yield return new WaitForSeconds(1f + _fadeSpeed);
+        FadeText(0f);
+        yield return new WaitForSeconds(_fadeSpeed);
+
+        while (_storyboard.m_SplitView >= -1)
+        {
+            _storyboard.m_SplitView -= _wipeSpeed * Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private void FadeText(float fade)
+    {
+        Sequence seq = DOTween.Sequence();
+        seq.Append(_chapterNameText.DOFade(fade, _fadeSpeed));
+        seq.Join(_chapterNumberText.DOFade(fade, _fadeSpeed));
+    }
+}
