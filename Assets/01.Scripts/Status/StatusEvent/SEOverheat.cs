e91ab6f7d52c54807a9fed865a09bbabb8de1a98
SimGyuYeong <shgy5901@gmail.com>
2023-05-20 15:50:09 +0900

    발열 추가 (아까는 과열추가엿음 ㅎㅎ)

--- Assets/01.Scripts/Status/StatusEvent/SEOverheat.cs
+++ Assets/01.Scripts/Status/StatusEvent/SEOverheat.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class SEOverheat : StatusEvent
 {
-    [SerializeField] private int _conditionCount = 10;
+    [SerializeField] private int _lessThanCount = 5;
+    [SerializeField] private int _overstackConditionCount = 10;
 
     public override void Invoke()
     {
-        if (_status.TypeValue >= _conditionCount)
+        if (_status.TypeValue >= _overstackConditionCount)
         {
             _unit.StatusManager.AddStatus(StatusName.Fire, _status.TypeValue / 2);
             _unit.StatusManager.DeleteStatus(_status);
+            _unit.StatusManager.DeleteStatus(StatusName.Heating);
+        }
+
+        if(_status.TypeValue < _lessThanCount)
+        {
+            _unit.StatusManager.DeleteStatus(StatusName.Heating);
         }
     }
 }
