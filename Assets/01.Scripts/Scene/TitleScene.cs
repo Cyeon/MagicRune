78b84ad02a75915f09c8544473fe7211bf785cda
SimGyuYeong <shgy5901@gmail.com>
2023-05-30 19:08:44 +0900

    씬 로딩화면 만듬

--- Assets/01.Scripts/Scene/TitleScene.cs
+++ Assets/01.Scripts/Scene/TitleScene.cs
@@ -1,11 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TitleScene : BaseScene
 {
+    [SerializeField]
+    private LoadScene _loadScene;
+
     public override void Clear()
     {
 
+    }
+
+    public void GameStart()
+    {
+        Managers.Clear();
+        StartCoroutine(_loadScene.LoadSceneCoroutine("LobbyScene"));
     }
 }
