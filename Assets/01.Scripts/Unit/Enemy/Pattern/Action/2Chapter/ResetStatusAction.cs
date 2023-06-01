3360cdfd252ea352770bc48414589c316ecc2e58
SimGyuYeong <shgy5901@gmail.com>
2023-06-01 12:21:58 +0900

    상태이상 쪽

--- Assets/01.Scripts/Unit/Enemy/Pattern/Action/2Chapter/ResetStatusAction.cs
+++ Assets/01.Scripts/Unit/Enemy/Pattern/Action/2Chapter/ResetStatusAction.cs
@@ -7,6 +7,6 @@ public class ResetStatusAction : PatternAction
     public override void TurnAction()
     {
         BattleManager.Instance.Enemy.StatusManager.Reset();
-        base.TurnAction();
+        base.TurnAction();                                                                                                                                                                                                           
     }
 }
