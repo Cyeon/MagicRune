cf6b0aeccb6aa0ba2ad0a5497d04771ea4222399
SimGyuYeong <shgy5901@gmail.com>
2023-06-05 16:15:33 +0900

    무속성 룬 2개 추가

---
+++ Assets/01.Scripts/Rune/Rune/Non/VariableRune.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableRune : BaseRune
+{
+    [HideInInspector]
+    public BaseRune nextRune;
+
+    public override void Init()
+    {
+        _baseRuneSO = Managers.Resource.Load<BaseRuneSO>("SO/Rune/Non/" + typeof(VariableRune).Name);
+    }
+
+    public override void AbilityAction()
+    {
+        if(nextRune.GetAbliltiValue(EffectType.Attack) != 0)
+        {
+            Managers.GetPlayer().Attack(GetAbliltiValue(EffectType.Attack), false);
+        }
+        else if(nextRune.GetAbliltiValue(EffectType.Defence) != 0)
+        {
+            Managers.GetPlayer().AddShield(GetAbliltiValue(EffectType.Defence));
+        }
+    }
+
+    public override object Clone()
+    {
+        VariableRune variableRune = new VariableRune();
+        variableRune.Init();
+        return variableRune;
+    }
+}
