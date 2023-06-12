2db99984ab8d1584fa900438dc11e6882d289552
JaePil Kim <kjp29816265@gmail.com>
2023-06-12 20:29:12 +0900

    아몰랑 버그 생성
    에셋 넣으면서 생긴거 같음

--- Assets/01.Scripts/UI/ChooseRunePanel.cs
+++ Assets/01.Scripts/UI/ChooseRunePanel.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class ChooseRunePanel : ExplainPanel
 {
+    [SerializeField]
+    private RectTransform _deckRectPos;
+    [SerializeField]
+    private GameObject _effect;
     public void ChooseRune()
     {
         //Managers.Deck.AddRune(_rune);
         Managers.Deck.AddRune(Managers.Rune.GetRune(_rune));
         Define.DialScene?.HideChooseRuneUI();
 
-        if (Managers.Reward.IsHaveNextClickReward())
+        // 이펙트 생성
+        // 이펙트가 도착하면 아래 코드를 작동
+        BezierMissile b = Managers.Resource.Instantiate("BezierMissile").GetComponent<BezierMissile>();
+        b.SetEffect(_effect);
+        b.Init(this.transform.position, Define.MainCam.ScreenToWorldPoint(_deckRectPos.anchoredPosition), 0.8f, 3, 3, () =>
         {
-            BattleManager.Instance.NextStage();
-        }
+            if (Managers.Reward.IsHaveNextClickReward())
+            {
+                BattleManager.Instance.NextStage();
+            }
+        });
     }
 
     public override void SetUI(BaseRune rune, bool isEnhance = true, bool isReward = true)
