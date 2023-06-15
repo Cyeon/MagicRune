44e67de2b8dbda64d92f5e1b436d5f33cd496a33
SimGyuYeong <shgy5901@gmail.com>
2023-06-16 00:05:23 +0900

    2스테이지 끝나면 게임종료

--- Assets/01.Scripts/Map/MapUI.cs
+++ Assets/01.Scripts/Map/MapUI.cs
@@ -9,6 +9,9 @@ public class MapUI : MonoBehaviour
 {
     private Image _mainBackground = null;
 
+    [Header("End Game")]
+    private GameObject _endGamePanel;
+
     private ChapterTransition _chapterTransition = null;
     public ChapterTransition ChapterTransition
     {
@@ -22,9 +25,14 @@ public class MapUI : MonoBehaviour
         }
     }
 
-    private void Start()
+    private void Awake()
     {
         _mainBackground = transform.Find("MainBackground").GetComponent<Image>();
+        _endGamePanel = transform.Find("EndGamePanel").gameObject;
+    }
+
+    private void Start()
+    {
         ChangeBackground();
     }
 
@@ -33,5 +41,20 @@ public class MapUI : MonoBehaviour
         string path = "Sprite/MapBg_" + Managers.Map.Chapter.ToString();
         if(_mainBackground != null)
             _mainBackground.sprite = Resources.Load<Sprite>(path);
+    }
+
+    public void EndGame()
+    {
+        _endGamePanel.SetActive(true);
+        transform.GetComponent<Canvas>().sortingLayerName = "Top";
+    }
+
+    public void GameExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
