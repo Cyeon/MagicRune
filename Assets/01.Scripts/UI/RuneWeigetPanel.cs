3573cefb862310ee5af82fe6ce06a4bf396a2dc2
JaePil Kim <kjp29816265@gmail.com>
2023-06-20 19:23:40 +0900

    다 만들기는 함
    야간의 조건처리 뺴고

--- Assets/01.Scripts/UI/RuneWeigetPanel.cs
+++ Assets/01.Scripts/UI/RuneWeigetPanel.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class RuneWeigetPanel : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _parent;
+
     private List<RuneWeiget> _wegetList = new List<RuneWeiget>();
 
     private RuneWeiget _select = null;
@@ -33,5 +36,17 @@ public class RuneWeigetPanel : MonoBehaviour
         GetComponentsInChildren<RuneWeiget>(_wegetList);
 
         _select = null;
+    }
+
+    public void SetActive(bool value)
+    {
+        if(Managers.Scene.CurrentScene.SceneType != Define.Scene.DialScene)
+        {
+            _parent.gameObject.SetActive(value);
+        }
+        else
+        {
+            _parent.gameObject.SetActive(false);
+        }
     }
 }
