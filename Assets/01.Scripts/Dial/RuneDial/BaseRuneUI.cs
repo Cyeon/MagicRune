8050498cfa3426c2a329453dfa8cb47569b5684f
JaePil Kim <kjp29816265@gmail.com>
2023-08-08 11:39:30 +0900

    키워드 안뜨더거 수정, 강화룬 테두리

--- Assets/01.Scripts/Dial/RuneDial/BaseRuneUI.cs
+++ Assets/01.Scripts/Dial/RuneDial/BaseRuneUI.cs
@@ -9,9 +9,13 @@ public class BaseRuneUI : MonoBehaviour
     private BaseRune _rune;
     public BaseRune Rune => _rune;
 
+    private SpriteRenderer _enhanceSr;
+
     void Start()
     {
         _runeSpriteRenderer = transform.Find("Sprite").GetComponent<SpriteRenderer>();
+        _enhanceSr = transform.Find("Enhance").GetComponent<SpriteRenderer>();
+        //_enhanceSr.gameObject.SetActive(false);
         RuneColor(new Color(0.26f, 0.26f, 0.26f, 1f));
     }
 
@@ -25,10 +29,16 @@ public class BaseRuneUI : MonoBehaviour
         }
 
         _runeSpriteRenderer.sprite = _rune.BaseRuneSO.RuneSprite;
+        if(_enhanceSr == null)
+        {
+            _enhanceSr = transform.Find("Enhance").GetComponent<SpriteRenderer>();
+        }
+        _enhanceSr.gameObject.SetActive(rune.IsEnhanced);
     }
 
     public void RuneColor(Color color)
     {
         _runeSpriteRenderer.color = color;
+        _enhanceSr.color = color;
     }
 }
--- Assets/01.Scripts/Scene/DialScene.cs
+++ Assets/01.Scripts/Scene/DialScene.cs
@@ -268,6 +268,7 @@ public class DialScene : BaseScene
     {
         if (rune == null) return;
 
+        _cardDescPanel.SetRune(rune);
         _cardDescPanel.SetUI(rune.BaseRuneSO, rune != null ? rune.IsEnhanced : false);
         _cardDescPanel.KeywardSetting();
     }
