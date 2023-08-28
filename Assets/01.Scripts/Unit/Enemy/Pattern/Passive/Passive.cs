15640a0493d6945220f3fc666f29d6a41169c857
SimGyuYeong <shgy5901@gmail.com>
2023-08-28 20:20:12 +0900

    패시브 테스트 끝

--- Assets/01.Scripts/Unit/Enemy/Pattern/Passive/Passive.cs
+++ Assets/01.Scripts/Unit/Enemy/Pattern/Passive/Passive.cs
@@ -11,6 +11,7 @@ public abstract class Passive : MonoBehaviour
     protected Enemy Enemy => Managers.Enemy.CurrentEnemy;
 
     public Sprite passiveIcon;
+    [Tooltip("** 뒤에 (패시브) 붙이기 **")]
     public string passiveName;
     [TextArea(0, 10)]
     public string passiveDescription;
