90bd9dae52b2a10ba89ac35faadf7dc845be7f0b
SimGyuYeong <shgy5901@gmail.com>
2023-08-29 12:24:48 +0900

    버그해결

--- Assets/01.Scripts/Unit/Enemy/Pattern/Passive/SnakeScale.cs
+++ Assets/01.Scripts/Unit/Enemy/Pattern/Passive/SnakeScale.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SnakeScale : Passive
 {
     [SerializeField] private int _increasePercent = 50;
-    public int increasedPerncent = 0;
+    private int _increasedPerncent = 0;
 
     public override void Disable()
     {
@@ -20,20 +20,20 @@ public class SnakeScale : Passive
         Enemy.OnTurnEnd.AddListener(IncreaseCheck);
         Enemy.OnGetDamage += DamageIncrease;
 
-        passiveDescription = "데미지를 받지 않으면 영구적으로 공격력 50%가 상승합니다.\r\n방어력은 턴이 지나도 사라지지 않습니다.\r\n현재 추가 공격력: " + _increasePercent + "%";
+        passiveDescription = "데미지를 받지 않으면 영구적으로 공격력 50%가 상승합니다.\r\n방어력은 턴이 지나도 사라지지 않습니다.\r\n현재 추가 공격력: " + _increasedPerncent + "%";
     }
 
     private void IncreaseCheck()
     {
         if(Enemy.currentDmg == 0)
         {
-            increasedPerncent += _increasePercent;
+            _increasedPerncent += _increasePercent;
             
         }
     }
 
     private void DamageIncrease()
     {
-        Enemy.currentDmg += Enemy.currentDmg * (increasedPerncent / 100);
+        Enemy.currentDmg += Enemy.currentDmg * (_increasedPerncent / 100);
     }
 }
