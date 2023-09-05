35792d63c5aa85ae31b4a5f573e9e4baf6b7e798
SimGyuYeong <shgy5901@gmail.com>
2023-09-05 09:53:56 +0900

    패턴 수정, 결과창 승패 메세지

--- Assets/01.Scripts/Unit/Enemy/Pattern/Action/ResetStatusAction.cs
+++ Assets/01.Scripts/Unit/Enemy/Pattern/Action/ResetStatusAction.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ResetStatusAction : PatternAction
 {
+    public override string Description => Define.BENEFIC_DESC;
+
     public override void TurnAction()
     {
         BattleManager.Instance.Enemy.StatusManager.Reset();
