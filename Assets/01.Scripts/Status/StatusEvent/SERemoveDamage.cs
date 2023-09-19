b2808ec0e3d8346219c06080b55b64cb524eeb05
SimGyuYeong <shgy5901@gmail.com>
2023-09-19 19:11:31 +0900

    금강불괴, 불 슬라임 버그해결

--- Assets/01.Scripts/Status/StatusEvent/SERemoveDamage.cs
+++ Assets/01.Scripts/Status/StatusEvent/SERemoveDamage.cs
@@ -18,6 +18,7 @@ public class SERemoveDamage : StatusEvent
     private int _value = 0;
 
     [SerializeField] private bool _isSelf = false;
+    [SerializeField] private bool _isAttack = false; // 데미지 감소가 공격할땐지, 공격을 받을땐지 체크
 
     public override void Invoke()
     {
@@ -32,11 +33,17 @@ public class SERemoveDamage : StatusEvent
 
         if (removeType == RemoveType.Percent)
         {
-            unit.attackDamage -= (int)(unit.attackDamage * (_percent / 100));
+            if(_isAttack)
+                unit.attackDamage -= (int)(unit.attackDamage * (_percent / 100));
+            else
+                unit.takeDamage -= (int)(unit.takeDamage * (_percent / 100));
         }
         else
         {
-            unit.attackDamage -= _value;
+            if (_isAttack)
+                unit.attackDamage -= _value;
+            else
+                unit.takeDamage -= _value;
         }
     }
 }
